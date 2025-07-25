4c74528cdb9ba26a1e5c360184bede63cef5a7ee
azher ali <azhermurad@gmail.com>
2025-07-25 13:02:31 +0500

    Multi-Source AI Knowledge Agent

--- KnowGenieAgent.py
+++ KnowGenieAgent.py
@@ -16,13 +16,22 @@ from langchain import hub
 from langchain_community.callbacks.streamlit import (
     StreamlitCallbackHandler,
 )
+import tempfile
+from langchain_community.document_loaders import PyPDFLoader
+from dotenv import load_dotenv
+import hashlib
 
 
-st.title("Chat With Search (Wiki,Arxiv,And Azher Ali(RAG))")
+load_dotenv()
+
+
+st.title("Multi-Source AI Knowledge Agent -- Search Wiki, Arxiv & Custom PDF Data")
 st.set_page_config(layout="wide")
-"""This multi-source agent empowers users to explore topics with
-🔎 trusted,  up-to-date, and  personalized information —
-perfect for  students,  researchers, and  professionals!"""
+"""This intelligent AI agent empowers users to explore information from multiple trusted sources — including Wikipedia, Arxiv, and personalized PDF documents — all in one seamless chat interface. By combining real-time search with Retrieval-Augmented Generation (RAG), it delivers accurate, context-aware answers tailored to your needs.
+
+Ideal for researchers, students, and professionals seeking instant insights from scholarly articles, encyclopedic knowledge, and custom data — without switching tabs.
+
+"""
 
 # page with
 
@@ -30,9 +39,59 @@ perfect for  students,  researchers, and  professionals!"""
 # api key
 with st.sidebar:
     st.title("Settings")
+    os.environ["GROQ_API_KEY"] = st.text_input(
+        "API KEY OF GROQ", type="password", value=os.getenv("GROQ_API_KEY")
+    )
+    # Upload PDF
+    uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
 
-    os.environ["GROQ_API_KEY"] = st.text_input("API KEY OF GROQ", type="password")
-    x = st.slider("x")
+    def get_file_hash(file_bytes):
+        return hashlib.md5(file_bytes).hexdigest()
+
+    if uploaded_file:
+        file_bytes = uploaded_file.read()
+        file_hash = get_file_hash(file_bytes)
+        if st.session_state.get("last_file_hash") != file_hash:
+            print("this function only called when we change the file")
+            st.session_state["last_file_hash"] = file_hash
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
+                uploaded_file.seek(0)
+                tmp_file.write(uploaded_file.read())
+                tmp_file_path = tmp_file.name
+
+            # Load PDF with LangChain
+            loader = PyPDFLoader(tmp_file_path)
+            pages = loader.load()
+
+            embeddings = HuggingFaceEmbeddings(
+                model_name="sentence-transformers/all-MiniLM-L6-v2",
+                model_kwargs={"device": "cpu"},
+            )
+
+            # load document
+            loader = WebBaseLoader("https://github.com/azhermurad")
+            docs = loader.load()
+
+            # split document into chunks
+            documents = RecursiveCharacterTextSplitter(
+                chunk_size=1000, chunk_overlap=200
+            ).split_documents(docs)
+
+            # vector store
+            vector = FAISS.from_documents(documents, embeddings)
+
+            # retriver
+            retriever = vector.as_retriever()
+
+            st.session_state["retriever"] = retriever
+
+            retriever_tool = create_retriever_tool(
+                st.session_state.retriever,
+                "personal_infomation_search",
+                "Search for information about azher ali. For any questions about azher ali, you must use this tool!",
+            )
+        else:
+            print("same file")
 
 if "messages" not in st.session_state:
     st.session_state.messages = [
@@ -51,73 +110,49 @@ for msg in st.session_state.messages:
         st.write(msg["content"])
 
 
-print(bool(os.environ["GROQ_API_KEY"]))
-if input := st.chat_input(
-    "Say something!", disabled=not bool(os.environ["GROQ_API_KEY"])
-):
-    # llm model
-    llm = ChatGroq(model="llama-3.1-8b-instant")
-
-    if "retriever" not in st.session_state:
-        print("in the retriver function")
-        # embedding
-        embeddings = HuggingFaceEmbeddings(
-            model_name="sentence-transformers/all-MiniLM-L6-v2",
-            model_kwargs={"device": "cpu"},
-        )
-
-        # load document
-        loader = WebBaseLoader("https://github.com/azhermurad")
-        docs = loader.load()
-
-        # split document into chunks
-        documents = RecursiveCharacterTextSplitter(
-            chunk_size=1000, chunk_overlap=200
-        ).split_documents(docs)
-
-        # vector store
-        vector = FAISS.from_documents(documents, embeddings)
-
-        # retriver
-        retriever = vector.as_retriever()
-
-        st.session_state["retriever"] = retriever
-
-    retriever_tool = create_retriever_tool(
-        st.session_state.retriever,
-        "personal_infomation_search",
-        "Search for information about azher ali. For any questions about azher ali, you must use this tool!",
-    )
-
-    # tools
-    wikipedia_tool = WikipediaQueryRun(
-        api_wrapper=WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=400)
-    )
-    arxiv_tool = ArxivQueryRun(
-        api_wrapper=ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=400)
-    )
-    tools = [wikipedia_tool, arxiv_tool, retriever_tool]
-
-    prompt = hub.pull("hwchase17/openai-functions-agent")
-    # create agent
-    agent = create_tool_calling_agent(llm, tools, prompt)
-    agent_executor = AgentExecutor(
-        agent=agent, tools=tools, verbose=True, handle_parsing_errors=True
-    )
-
-    st.session_state.messages.append({"role": "user", "content": input})
-    with msg_container.chat_message(
-        "user",
-        avatar="https://media.licdn.com/dms/image/v2/D4D03AQELrOU4Hf6Jeg/profile-displayphoto-scale_100_100/B4DZf.QMXBHkAg-/0/1752317351587?e=1756339200&v=beta&t=aVlXfjLRnFJU2pkBU_g_nlPYTpyfQdPEtq4tX3glA_c",
+if bool(os.environ["GROQ_API_KEY"]):
+    if input := st.chat_input(
+        "Say something!", disabled=not bool(os.environ["GROQ_API_KEY"])
     ):
-        st.write(input)
+        # llm model
+        llm = ChatGroq(model="llama-3.1-8b-instant")
 
-    with st.chat_message("assistant"):
-        st_callback = StreamlitCallbackHandler(st.container())
-        response = agent_executor.invoke({"input": input}, {"callbacks": [st_callback]})
-        st.session_state.messages.append(
-            {"role": "assistant", "content": response["output"]}
+        # tools
+        wikipedia_tool = WikipediaQueryRun(
+            api_wrapper=WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=400)
         )
-        st.write(response["output"])
+        arxiv_tool = ArxivQueryRun(
+            api_wrapper=ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=400)
+        )
+
+        tools = (
+            [wikipedia_tool, arxiv_tool, retriever_tool]
+            if st.session_state.get("retriever")
+            else [wikipedia_tool, arxiv_tool]
+        )
+
+        prompt = hub.pull("hwchase17/openai-functions-agent")
+        # create agent
+        agent = create_tool_calling_agent(llm, tools, prompt)
+        agent_executor = AgentExecutor(
+            agent=agent, tools=tools, verbose=True, handle_parsing_errors=True
+        )
+
+        st.session_state.messages.append({"role": "user", "content": input})
+        with msg_container.chat_message(
+            "user",
+            avatar="https://media.licdn.com/dms/image/v2/D4D03AQELrOU4Hf6Jeg/profile-displayphoto-scale_100_100/B4DZf.QMXBHkAg-/0/1752317351587?e=1756339200&v=beta&t=aVlXfjLRnFJU2pkBU_g_nlPYTpyfQdPEtq4tX3glA_c",
+        ):
+            st.write(input)
+
+        with st.chat_message("assistant"):
+            st_callback = StreamlitCallbackHandler(st.container())
+            response = agent_executor.invoke(
+                {"input": input}, {"callbacks": [st_callback]}
+            )
+            st.session_state.messages.append(
+                {"role": "assistant", "content": response["output"]}
+            )
+            st.write(response["output"])
 else:
     st.warning("API key is missing!!!")
